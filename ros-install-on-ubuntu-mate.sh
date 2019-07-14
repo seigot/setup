b9eccb6e0c6abeeffc99bb3d5a7207a1a555424a
seigot <46782860+seigot@users.noreply.github.com>
2019-07-14 12:54:54 +0900

    ros install
    ros install

---
+++ ros-install-on-ubuntu-mate.sh
@@ -0,0 +1,40 @@
+#!/bin/sh 
+
+## general
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get autoremove
+
+## Step 3: Setup your sources.list
+sudo sh -c ‘echo “deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main” > /etc/apt/sources.list.d/ros-latest.list’
+
+## Step 4: Setup your keys
+wget http://packages.ros.org/ros.key -O – | sudo apt-key add –
+
+## Step 5: To be sure that your Ubuntu Mate package index is up to date, type the following command
+sudo apt-get update
+
+## Step 6: Install ros-kinetic-desktop-full
+sudo apt-get install ros-kinetic-desktop-full
+
+## Step 7: Initialize rosdep
+sudo rosdep init
+rosdep update
+
+## Step 8: Setting up the ROS environment variables
+echo “source /opt/ros/kinetic/setup.bash” >> ~/.bashrc
+source ~/.bashrc
+
+## Step 9: Create and initialize the catkin workspace
+mkdir -p ~/catkin_workspace/src
+cd catkin_workspace/src
+catkin_init_workspace
+cd ~/catkin_workspace/
+catkin_make
+
+## Step 10: Add the catkin_workspace to your ROS environment
+source ~/catkin_workspace/devel/setup.bash
+echo “source ~/catkin_workspace/devel/setup.bash” >> ~/.bashrc
+
+## Step 11: Check the ROS environment variables
+export | grep ROS
